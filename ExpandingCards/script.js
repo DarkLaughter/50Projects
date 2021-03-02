5e5e67ef167d1babd084b17c7e65a2f5e72494de
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-03-01 23:49:43 -0500

    script js finalized and css update

---
+++ ExpandingCards/script.js
@@ -0,0 +1,15 @@
+//Grab all divs with panel class 
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
