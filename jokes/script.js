33f0417cb9b014d0d30d4df74288b0717b5f6162
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-03-29 23:54:28 -0400

    updated fetch to use async and await

--- jokes/script.js
+++ jokes/script.js
@@ -5,17 +5,15 @@ btn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+async function generateJoke() {
     const config = {
         headers: {
         'Accept': 'application/json'
     },
 }
 
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+    jokeElement.innerHTML = data.joke
 
-    fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-        jokeElement.innerHTML = data.joke
-    })
 }
