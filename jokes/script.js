d0a845ecf113a8bcd1f95218ff42757433677442
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-03-29 23:44:59 -0400

    fetch api test

---
+++ jokes/script.js
@@ -0,0 +1,18 @@
+const jokeElement = document.getElementById('#joke')
+const btn = document.getElementById('#jokeBtn')
+
+generateJoke()
+
+function generateJoke() {
+
+    const config = {
+        headers: {
+        'Accept': 'application/json'
+    },
+}
+
+
+    fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+}
