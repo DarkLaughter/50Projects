486c27e6fb5b456ddb7dbd2999380bd788800e07
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-04-11 21:27:06 -0400

    added main js functions for random tag selection

--- Randomizer/main.js
+++ Randomizer/main.js
@@ -4,6 +4,15 @@ const txtArea = document.getElementById('textarea')
 txtArea.focus()
 txtArea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
+
 })
 
 function createTags(input) {
@@ -19,4 +28,43 @@ function createTags(input) {
         tags.appendChild(tagEl)
     });
 
+}
+
+function randomSelect () {
+    const times = 30
+
+    const interval = setInterval(() => {
+
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            removeHighlightTag(randomTag)
+        }, 100)
+    }, 100)
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const random = pickRandomTag()
+            highlightTag(random)
+        }, 100)
+
+    }, times * 100)
+
+}
+
+
+function pickRandomTag () {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function removeHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
