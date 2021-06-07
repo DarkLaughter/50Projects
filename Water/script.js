168901e356cb4ef2f3e2beb7a8f88d33d01aed63
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-06-07 11:42:34 -0400

    smaller cups js added

--- Water/script.js
+++ Water/script.js
@@ -1,1 +1,25 @@
 // Js for Water directory
+
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')
+const per = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => {
+        highlight(idx)
+    })
+})
+
+function highlight(idx) {
+
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) idx--;
+
+    smallCups.forEach((cup, idx2) => {
+        if(idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+}
